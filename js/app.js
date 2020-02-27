2bc60d991a5de12ccc185c0f71775c2b4d4fd209
Jun Yang <JYang@ucvts.tec.nj.us>
2020-02-27 10:03:28 -0500

    js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 var canvas = documnet.getElementById("bboard");
 var ctx = canvas.getContext("2d");
+
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
+ctx.rect(40, 80, 100, 100);
+ctx.fillStyle = "#FF0000"
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeSty;e = "#000000"
+ctx.stroke();
