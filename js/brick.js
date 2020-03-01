acea00e45895cd723d2739f2ff3c630756821659
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-03-01 14:54:24 -0500

    even more js

--- js/brick.js
+++ js/brick.js
@@ -53,4 +53,23 @@ var canvas = document.getElementById("canvas_for_game");
 		ctx.closePath();
 	}
 
+	function drawBricks() {
+		for(c = 0; c < brickColumnCount; c++) {
+			for(r = 0; r < brickRowCount; r++) {
+				if(bricks[c][r].status == 1) {
+					var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+					bricks[c][r].x = brickX;
+					bricks[c][r].y = brickY;
+
+					ctx.beginPath();
+					ctx.rect(brickX, brickY, brickWidth, brickHeight);
+					ctx.fillStyle = brickColor;
+					ctx.fill();
+					ctx.closePath();
+				}
+			}
+		}
+	}
+
 	
